7594feeacfe54b718f6165f94829b86e2511a0db
Uria <dorman010@gmail.com>
2023-09-25 15:05:37 +0100

    Add navigation back home homepage via menu

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import './style.css'
+import { aboutPg } from "./menu"
+
 function addContent() {
     const textNode = document.createElement('div')
     textNode.id = 'main'
-    textNode.innerHTML = `<div id="menu"><div class="menuItem" id="about"><button class='menuBtn'>Mead Menu</button></div>
+    textNode.innerHTML = `<div id="menu"><div class="menuItem" id="homeBtn"><button class='menuBtn'>Home</button></div><div class="menuItem" id="about"><button class='menuBtn'>Mead Menu</button></div>
     <div class="menuItem" id="history"><button class='menuBtn'>Our History</button></div>
     <div class="menuItem" id="contact"><button class='menuBtn'>Contact</button></div></div>
     <div id='pageContent'><h1>Thames.mead</h1>
@@ -16,8 +19,19 @@ function createDiv() {
     divNode.id='content'
     document.body.appendChild(divNode)
 }
+
+function homeClick() {
+    const homeBtn = document.querySelector('#homeBtn')
+    homeBtn.addEventListener('click', function(e){
+        e.preventDefault();
+        document.getElementById('pageContent').innerHTML=`<h1>Thames.mead</h1>
+        <img src="https://images.unsplash.com/photo-1584225064785-c62a8b43d148" id='landingImg' style='height:450px;;object-fit:contain'>
+        <h2>As simple as it sounds - mead,  from Thamesmead.</h2>
+        <p>Thamesmead has a history of mead making at the Lesnes Abbey. We bring this ancient tradition back to life, using modern tools!</p>`;
+
+    })
+}
 createDiv()
 addContent()
-import { aboutPg } from "./menu"
 aboutPg()
-import './style.css'
+homeClick()
--- src/menu.js
+++ src/menu.js
@@ -1,21 +1,16 @@
 export function aboutPg(){
     const aboutBtn = document.querySelector('#about');
     aboutBtn.addEventListener('click', function() {
-        const mainBody = document.querySelector('#main');
-        mainBody.innerHTML = `<div id="menu"><div class="menuItem" id="homeBtn"><button class='menuBtn'>Home</button></div>
-        <div class="menuItem" id="about"><button class='menuBtn'>Mead Menu</button></div>
-        <div class="menuItem" id="history"><button class='menuBtn'>Our History</button></div>
-        <div class="menuItem" id="contact"><button class='menuBtn'>Contact</button></div></div>
-        <div id='pageContent'><h1>About</h1>
+        const mainBody = document.querySelector('#pageContent');
+        mainBody.innerHTML = `<h1>The Mead Menu</h1>
         <h2>Bringing ancient traditions to life with modern tools.</h2>
-        <p>Honey wine - or mead as it is colloquially called - is one of the first forms of alcohol consumed by humans. We are a collection of home brewers who try to share this ancient tradition with others, using modern tools. Our mead selection includes:</p>
+        <p>Honey wine - commonly called mead - is one of the first forms of alcohol consumed by humans. We are a collection of home brewers who try to share this ancient tradition with others, using modern tools. Our mead selection includes:</p>
         <div id="menu">
             <div id="meadList"><div class="header">Mead name</div><div class="header">Tasting notes</div><div class="header">ABV</div><div class="header">Price</div><div class="header">Bottle size</div>
             <div class="entry">"The Trappist"</div><div class="entry">Lemony, zesty with hints of spice</div><div class="entry">11.8%</div><div class="entry">£19.99</div><div class="entry">75cl</div>
             <div class="entry">"The Sack"</div><div class="entry">Sweet, light, hints of caramel</div><div class="entry">15.5%</div><div class="entry">£24.99</div><div class="entry">75cl</div>
             <div class="entry">"Killer Bee"</div><div class="entry">Spicy and zesty, with hints of pepper</div><div class="entry">12%</div><div class="entry">£14.99</div><div class="entry">50cl</div>
             <div class="entry">"Cyser Squad"</div><div class="entry">Tart, sweet with hints of vanilla</div><div class="entry">10%</div><div class="entry">£9.99</div><div class="entry">33cl</div></div>
-        </div>        
         </div>`
     })
 }
